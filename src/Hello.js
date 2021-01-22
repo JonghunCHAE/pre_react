7f6dd8c9990fbcadf718ba63f391cfa035943f76
JonghunCHAE <ace1373@naver.com>
2021-01-22 16:50:34 +0900

    24강 클래스 컴포넌트 완료

--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Hello({color, name, isSpecial}) {
-    return (
-        <div style={{ color }}>
-            {isSpecial && <b>*</b>}
-            안녕하세요 {name}
-        </div>
-    );
-}
+class Hello extends Component {
+    static defaultPros = {
+        name: '이름없음'
+    };
+    
+    render() {
+        const {color, name, isSpecial} = this.props;
 
-Hello.defaultProps = {
-    name: '이름없음'
+        return (
+            <div style={{ color }}>
+                {isSpecial && <b>*</b>}
+                안녕하세요 {name}
+            </div>
+        )
+    }
 }
 
 export default Hello;
